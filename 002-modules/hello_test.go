c8d65237f73cec56fae68e68685da3f87c33d8ef
Fermin <fermin@eventbrite.com>
2020-04-26 20:07:13 +0200

    play with dependency

--- 002-modules/hello_test.go
+++ 002-modules/hello_test.go
@@ -8,3 +8,10 @@ func TestHello(t *testing.T) {
 		t.Error("Hello()")
 	}
 }
+
+func TestProverb(t *testing.T) {
+    want := "Concurrency is not parallelism."
+    if got := Proverb(); got != want {
+        t.Errorf("Proverb() = %q, want %q", got, want)
+    }
+}
