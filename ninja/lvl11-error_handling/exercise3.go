07dbb91ecf4b2803d9fa9679a9703ec2b9e39dd9
Fermin <fermin.hdez@gmail.com>
2020-06-01 17:49:32 +0200

    Error handling examples

---
+++ ninja/lvl11-error_handling/exercise3.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type customErr struct {
+	info string
+}
+
+
+func (ce customErr) Error() string {
+	return fmt.Sprintf("what %v", ce.info)
+}
+
+func main() {
+	c1 := customErr{info: "need more coffee"}
+	foo(c1)
+}
+
+func foo(e error) {
+	fmt.Println("foo ran -", e, "\n", e.(customErr).info)
+}
